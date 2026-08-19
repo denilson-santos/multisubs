99a637119bae1ca16818b075a4b42122f36ffe1d
Denilson da Silva Santos <denilson.silvasantos.contato@gmail.com>
2026-08-19 12:28:04 -0300

    chore: bump version to 1.0.1

--- multisubs/__init__.py
+++ multisubs/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = ("__version__", "embed_subtitles", "generate_transcriptions")
 
